8e19638d91ca253c37791d7ce6881223fc581a64
Sebzzzo <juansgarcia1704@gmail.com>
2025-04-11 08:22:28 -0500

    NuevoMain
    Cree e l nuevo main

---
+++ src/juan/Rodriguez/JuanDavidRodriguez.java
@@ -0,0 +1,5 @@
+package Juan.Rodriguez;
+
+public class JuanDavidRodriguez {
+	
+}
---
+++ src/juan/Rodriguez/MainJuanDavidRodriguez.java
@@ -0,0 +1,10 @@
+package Juan.Rodriguez;
+
+public class MainJuanDavidRodriguez {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
